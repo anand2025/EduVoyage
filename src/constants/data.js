236579361772f321364de4d8ee0520fb7f27183b
Shruti Anand <105790879+anand2025@users.noreply.github.com>
2023-12-22 16:43:03 +0530

    Delete src directory

--- src/constants/data.js
+++
@@ -1,8 +0,0 @@
-//different categories of blogs
-export const categories = [
-    { id: 1, type: "Academic Life" },
-    { id: 2, type: "Campus Life" },
-    { id: 3, type: "Career" },
-    { id: 4, type: "Technology" },
-    { id: 5, type: "Communities" }
-];
