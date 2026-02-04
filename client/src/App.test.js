bfbdfb08bf50eae87101371cc10833324f16a032
shrutianand200402 <shrutianand200402@gmail.com>
2026-02-04 23:11:10 +0530

    test: fix App test failure and resolve dependency issues
    - Restore testing library dependencies to stable versions compatible with CRA 5/React 18.
    - Update [App.test.js](cci:7://file:///c:/Users/ASUS/EduVoyage/client/src/App.test.js:0:0-0:0) to match landing page content and rename test to 'renders landing page'.
    - Opt-in to React Router v7 future flags in [App.js](cci:7://file:///c:/Users/ASUS/EduVoyage/client/src/App.js:0:0-0:0) to silence deprecation warnings.
    - Synchronize `package-lock.json` with updated dependency structure.
    
    Signed-off-by: shrutianand200402 <shrutianand200402@gmail.com>

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders landing page', () => {
   render(<App />);
   const linkElement = screen.getByText(/Welcome to EduVoyage/i);
   expect(linkElement).toBeInTheDocument();
